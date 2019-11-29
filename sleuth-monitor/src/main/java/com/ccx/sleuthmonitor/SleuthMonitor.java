cb94dd73a118933857351e68bd43385d533c2f2c
ccx1016875161@126.com <ccx1016875161@gmail.com>
2019-11-29 14:08:02 +0800

    实现了将链路跟踪输出到rabbitmq中，并持久化到mysql中去

--- sleuth-monitor/src/main/java/com/ccx/sleuthmonitor/SleuthMonitor.java
+++ sleuth-monitor/src/main/java/com/ccx/sleuthmonitor/SleuthMonitor.java
@@ -2,6 +2,7 @@ package com.ccx.sleuthmonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import zipkin.server.EnableZipkinServer;
